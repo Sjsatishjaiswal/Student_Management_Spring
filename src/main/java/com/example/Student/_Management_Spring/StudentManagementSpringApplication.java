f90d1e7178ddaca7e5287386f37df9668c3b34ab
Satish Jaiswal <Sjsatishjaiswalcse2017@gmail.com>
2023-03-21 23:51:29 +0530

    Student Spring API Project

---
+++ src/main/java/com/example/Student/_Management_Spring/StudentManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Student._Management_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSpringApplication.class, args);
+	}
+
+}
